dd6f54c0b43c61bcb56965a437b183c2f1542d5d
JoaoEnrique13 <joao.alves254@etec.sp.gov.br>
2024-02-28 18:31:55 -0300

    Rotas Node

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+
+//Callback - cria um servidor web
+app.listen(8081, function(){
+    console.log("Servidor rodando na porta 8081");
+})
+
+//home
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/html/index.html')
+})
+
+//cadastrar produto
+app.get('/cadastrar/produto', function(req, res){
+    res.sendFile(__dirname + '/html/cadastrar_produto.html')
+})
+
+//pesquisar produto
+app.get('/pesquisar/produto/modelo', function(req, res){
+    res.sendFile(__dirname + '/html/pesquisar_produto.html')
+})
+
+//contato produto
+app.get('/contato/sac/produto', function(req, res){
+    res.sendFile(__dirname + '/html/contato.html')
+})
